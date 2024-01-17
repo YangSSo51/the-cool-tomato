8602bbe55c7d7924b47cba1e38b07d7921be2329
hyeseungS <hyeseung2000@gmail.com>
2024-01-17 12:48:13 +0900

    ✨ Feat: User 엔티티 추가

---
+++ src/main/java/com/wp/user/domain/user/entity/User.java
@@ -0,0 +1,52 @@
+package com.wp.user.domain.user.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+// 회원 기본 정보
+public class User {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(length = 10, nullable = false)
+    private String auth;
+
+    @Column(length = 50, nullable = false, unique = true)
+    private String loginId;
+
+    @Column(nullable = false)
+    @JsonIgnore
+    private String password;
+
+    @Column(length = 100, unique = true)
+    private String email;
+
+
+    @Column(length = 50)
+    private String nickname;
+
+    @Column(columnDefinition = "TINYINT", length = 1)
+    private Sex sex;
+
+    private LocalDate birthday;
+
+    private String profileImg;
+
+    @CreatedDate
+    private LocalDateTime joinDate;
+}
