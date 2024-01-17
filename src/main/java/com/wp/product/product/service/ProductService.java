e1f25f568bb4a15bd9211d337cefc287372f9347
YangSSo51 <48993188+YangSSo51@users.noreply.github.com>
2024-01-17 17:44:27 +0900

    feat : 상품 등록 기능 추가

---
+++ src/main/java/com/wp/product/product/service/ProductService.java
@@ -0,0 +1,7 @@
+package com.wp.product.product.service;
+
+import com.wp.product.product.dto.request.ProductRequest;
+
+public interface ProductService {
+    void saveProduct(ProductRequest productRequest);
+}
