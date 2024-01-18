0597394fee961485d01016f608064e3911409a5d
박호철 <didifia9850@naver.com>
2024-01-18 18:01:32 +0900

    Feat: Itemlist Update

--- vite-project/src/components/item/dummylist/dummy.tsx
+++ vite-project/src/components/item/dummylist/dummy.tsx
@@ -2,7 +2,6 @@ const Goodslist = [
     {
         id: 0,
         title: "달콤보들 프렌치토스트",
-        content: "간단히 만드는 대표 브런치 메뉴",
         price: 120000,
         img: "/img/curry.png",
     },
@@ -10,7 +9,6 @@ const Goodslist = [
     {
         id: 1,
         title: "실패 없는 카레 만드는 법",
-        content: "재료 얼만큼 넣어야 할지 몰라 항상 실패했다면?",
         price: 110000,
         img: "https://shopping-phinf.pstatic.net/main_8143847/81438478966.4.jpg",
     },
@@ -18,7 +16,6 @@ const Goodslist = [
     {
         id: 2,
         title: "봉골레 파스타",
-        content: "조개의 시원한 감칠맛을 담은 요리",
         price: 130000,
         img: "/img/Toast.png",
     },
@@ -26,7 +23,6 @@ const Goodslist = [
     {
         id: 3,
         title: "홈메이드 투움바 파스타",
-        content: "매콤하고 꾸덕하게 즐기는 크림의 맛",
         price: 130000,
         img: "/img/PA.png",
     },
@@ -34,9 +30,29 @@ const Goodslist = [
     {
         id : 4,
         title : "감바스 알 아히요",
-        content : "요즘 파티 요리에 빠지지 않는 새우요리",
         price : 10000,
         img: "/img/gambase.png",
+      },
+
+      {
+        id : 5,
+        title : "야미",
+        price : 100000,
+        img : "https://shopping-phinf.pstatic.net/main_8716119/87161199873.2.jpg"
+      },
+
+      {
+        id : 6,
+        title : "소시지",
+        price : 100000,
+        img : "https://shopping-phinf.pstatic.net/main_8251676/82516769783.3.jpg"
+      },
+
+      {
+        id : 7,
+        title : "김현종",
+        price : 1000200,
+        img : "https://shopping-phinf.pstatic.net/main_8704626/87046261628.jpg"
       }
 ];
 
--- vite-project/src/pages/ItemList.tsx
+++ vite-project/src/pages/ItemList.tsx
@@ -1,14 +1,21 @@
 import { Box, Flex } from "@chakra-ui/react";
 import Goods from "../components/item/ItemListComponent";
 import GoodsList from "../components/item/dummylist/dummy";
+import CarouselComponent from "../components/item/CarousuelComponent";
+import "../css/ItemListComponentcss.css";
 
 export default function ItemList() {
     const dummylist = GoodsList;
     return (
         <>
-        <Flex maxW={"100%"}>
-
-        </Flex>
+            <Flex
+                justify={"center"}
+                className="MainText"
+                color={"themeGreen.500"}
+            >
+                현재 진행중인 라이브
+            </Flex>
+            <CarouselComponent />
             <Flex wrap="wrap" maxW="1280px" m="auto" gap={6}>
                 {dummylist.map((data) => (
                     <Box key={data.id} w="calc(23.33%)" p={4}>
