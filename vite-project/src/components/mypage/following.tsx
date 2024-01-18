df43867a60a9e5ad926ac7edf737f3e853303d11
김현지 <hanguel97@gmail.com>
2024-01-18 17:20:15 +0900

    feat: 컴포넌트 내용 수정

--- vite-project/src/components/mypage/following.tsx
+++ vite-project/src/components/mypage/following.tsx
@@ -1,5 +1,5 @@
-import { Box, Flex, Text } from "@chakra-ui/layout";
-import { useState } from "react";
+import { Box, Heading, Text, Flex, Badge } from "@chakra-ui/layout";
+import { Avatar } from "@chakra-ui/react";
 // import { useQuery } from "@tanstack/react-query";
 // import axios from "axios";
 
@@ -10,58 +10,38 @@ export default function Following() {
     //     .then((a)=> {return a.data})
     // )
 
-    const [ currentSee, setCurrentSee ] = useState([ {
-        "product_id" : 3,
-        "category_id" : 1,
-        "seller_id" : 1,
-        "product_name" : "Followingname1",
-        "product_content" : "only 5 inches",
-        "price" : 1000
-      },
-      {
-        "product_id" : 2,
-        "category_id" : 2,
-        "seller_id" : 3,
-        "product_name" : "Followingname2",
-        "product_content" : "for less than 6",
-        "price" : 20000
-      },
-      {
-        "product_id" : 1,
-        "category_id" : 3,
-        "seller_id" : 5,
-        "product_name" : "Followingname3",
-        "product_content" : "Born in France",
-        "price" : 6000
-      }
-    ])
-
-    const testData = currentSee.map((item, index) => {
-        return (
-            <li key={index}>{item.product_name} / {item.product_content}</li>
-        )
-    })
-
     return (
         <Box w="75%" bg="white" rounded="lg" overflow="hidden">
-            <Box h="full" pl="4">
-                <Flex justify="center" align="center" h="full">
-                { testData ? testData : <Text color="gray.400">Content goes here</Text> }
-                </Flex>
+            <Box h="full" pl={4}>
+                <Box h="full" flexDir="column" p={4}>
+                    <Heading size="lg" fontWeight="bold" mb={4}>
+                    팔로잉 목록
+                    </Heading>
+                    {Array.from({ length: 3 }).map((_, index) => (
+                    <Box key={index} w="full" borderBottom="1px" pb={4} mb={4} >
+                        <Avatar mt="4" size="xl" src="https://raw.githubusercontent.com/printilikepenguin/forUserContent/master/profile.png" />
+                        <Box>
+                        <Text color="gray.700" fontWeight="medium">
+                            userId{index + 1}
+                        </Text>
+                        <Text color="gray.500">nickname{index + 1}</Text>
+                        </Box>
+                    </Box>
+                    ))}
+                    <Flex>
+                        <Avatar src='https://bit.ly/sage-adebayo' />
+                        <Box ml='3'>
+                            <Text fontWeight='bold'>
+                            Segun Adebayo
+                            <Badge ml='1' colorScheme='green'>
+                                New
+                            </Badge>
+                            </Text>
+                            <Text fontSize='sm'>UI Engineer</Text>
+                        </Box>
+                    </Flex>
+                </Box>
             </Box>
         </Box>
-
     )
 }
-
-// function Card(props) {
-//     console.log(props)
-//     return (
-//       <div>
-//         <Link to={"/detail/" + (props.i)}>
-//         <img src={process.env.PUBLIC_URL + "/shop" + (props.i+1) + ".jpg"} width="80%" alt="상품 이미지" /></Link>
-//           <h4>{props.products.title}</h4>
-//           <p>{props.products.price}</p>
-//       </div>
-//     )
-//   }
--- vite-project/src/pages/BuyerPage.tsx
+++ vite-project/src/pages/BuyerPage.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { Box, Flex, Center } from "@chakra-ui/layout";
+import { Box, Flex, Center, Heading } from "@chakra-ui/layout";
 import { Button, Avatar, List, ListItem } from "@chakra-ui/react";
 import axios from "axios";
 
@@ -13,18 +13,18 @@ import Sellerform from "../components/mypage/Sellerform";
 
 export default function BuyerPage() {
 
-  const navigate = useNavigate();
-  const { userId } = useParams();
-  const [ userInfo, setUserInfo ] = useState([]);
-  const [ tab, setTab ] = useState(0);
-  const categoryTabs = [
-    { id: 0, name: '최근 본 상품', component: <Recent userId={userId} /> },
-    { id: 1, name: '팔로잉 목록', component: <Following userId={userId} /> },
-    { id: 2, name: '작성 가능한 리뷰', component: <Reviews userId={userId} /> },
-    { id: 3, name: '작성한 리뷰', component: <Reviewed userId={userId} /> },
-    { id: 4, name: '내가 한 문의', component: <Question userId={userId} /> },
-    { id: 5, name: '판매자 신청', component: <Sellerform userId={userId} /> },
-  ];
+    const navigate = useNavigate();
+    const { userId } = useParams();
+    const [ userInfo, setUserInfo ] = useState([]);
+    const [ tab, setTab ] = useState(0);
+    const categoryTabs = [
+        { id: 0, name: '최근 본 상품', component: <Recent userId={userId} /> },
+        { id: 1, name: '팔로잉 목록', component: <Following userId={userId} /> },
+        { id: 2, name: '작성 가능한 리뷰', component: <Reviews userId={userId} /> },
+        { id: 3, name: '작성한 리뷰', component: <Reviewed userId={userId} /> },
+        { id: 4, name: '내가 한 문의', component: <Question userId={userId} /> },
+        { id: 5, name: '판매자 신청', component: <Sellerform userId={userId} /> },
+    ];
 
     useEffect(() => {
         const fetchData = async () => {
@@ -37,7 +37,7 @@ export default function BuyerPage() {
                 // }
                 );
             setUserInfo(response.data);
-            console.log(userInfo[0])
+            console.log(userInfo)
             } catch (error) {
             console.error("Error fetching user data:", error);
             }
@@ -45,59 +45,61 @@ export default function BuyerPage() {
         fetchData();
     }, []);
 
-  return (
-    <Box bg="yellow" minH="100vh">
-      <Box>
-        <Center>마이페이지</Center>
-      </Box>
+    return (
+        <Box minH="100vh" mb="10">
+            <Flex mt="10" mb="10" justify="center">
 
-      <Flex m="auto" bg="black" rounded="lg" w="90vw" minH="90vh">
-        <Flex m="auto" bg="blue" rounded="lg" w="80vw" minH="80vh" px="2">
-          <Box w="25%" pr="4">
-            <Box w="full" bg="white" rounded="lg" overflow="hidden">
-              <Flex direction="column" align="center" py="6">
+            <Center w="30vw" bgImage="url('/icons/up_ttl.svg')"
+            ><Heading>마이페이지</Heading></Center>
+            </Flex>
 
-                <Button
-                  onClick={() => {
-                    navigate("/v1/seller");
-                  }}
-                >판매자 정보 보기
-                </Button>
+            <Flex m="auto" border="1px" borderColor="green" rounded="lg" w="90vw" minH="90vh">
+                <Flex m="auto" rounded="lg" w="80vw" minH="80vh" px="2">
+                    <Box w="25%" pr="4">
+                    <Box w="full" bg="white" rounded="lg" overflow="hidden">
+                        <Flex direction="column" align="center" py="6">
 
-                <Avatar mt="4" size="xl" src="https://raw.githubusercontent.com/printilikepenguin/forUserContent/master/profile.png" />
+                        <Button
+                            onClick={() => {
+                            navigate("/v1/seller");
+                            }}
+                        >판매자 정보 보기
+                        </Button>
 
-                <Button
-                  mt="4"
-                  onClick={() => {
-                    navigate("/v1/userinfo");
-                  }}
-                >계정정보수정
-                </Button>
+                        <Avatar mt="4" size="xl" src="https://raw.githubusercontent.com/printilikepenguin/forUserContent/master/profile.png" />
 
-                <Box w="full" mt="6" pt="6">
-                  <List spacing="4">
-                    {categoryTabs.map((category) => (
-                      <ListItem
-                        key={category.id}
-                        onClick={() => setTab(category.id)}
-                      >{category.name}
-                      </ListItem>
-                    ))}
-                  </List>
-                </Box>
-              </Flex>
-            </Box>
-          </Box>
+                        <Button
+                            mt="4"
+                            onClick={() => {
+                            navigate("/v1/userinfo");
+                            }}
+                        >계정정보수정
+                        </Button>
 
-          <Box w="75%" bg="white" rounded="lg" overflow="hidden">
-            <Box h="full" pl="4">
-              <Flex justify="center" align="center" h="full">
-                {categoryTabs[tab].component}
-              </Flex>
-            </Box>
-          </Box>
-        </Flex>
-      </Flex>
-    </Box>
-  );
+                        <Box w="full" mt="6" pt="6">
+                            <List spacing="4">
+                            {categoryTabs.map((category) => (
+                                <ListItem
+                                key={category.id}
+                                onClick={() => setTab(category.id)}
+                                >{category.name}
+                                </ListItem>
+                            ))}
+                            </List>
+                        </Box>
+                        </Flex>
+                    </Box>
+                    </Box>
+
+                    <Box w="75%" bg="white" rounded="lg" overflow="hidden">
+                    <Box h="full" pl="4">
+                        <Flex justify="center" align="center" h="full">
+                        {categoryTabs[tab].component}
+                        </Flex>
+                    </Box>
+                    </Box>
+                </Flex>
+            </Flex>
+        </Box>
+    );
 }
