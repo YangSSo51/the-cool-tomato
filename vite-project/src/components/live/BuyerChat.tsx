0069c02d547cd721daa522d7e786d1c90bc03e0a
박호철 <didifia9850@naver.com>
2024-01-23 15:46:28 +0900

    Feat: BroadCast Update

--- vite-project/src/components/live/BuyerChat.tsx
+++ vite-project/src/components/live/BuyerChat.tsx
@@ -1,11 +1,24 @@
-import { Center, Text } from "@chakra-ui/react"
+import { Box, Center, Flex, Text } from "@chakra-ui/react";
 
 export default function BuyerChat() {
     return (
         <>
-            <Center p={"3px"}>
-                <Text as={'b'} fontSize={'lg'}>실시간 채팅</Text>
-            </Center>
+            <Flex direction={"column"} h={"100%"} p={"1rem"}>
+                <Center>
+                    <Text as={"b"} fontSize={"lg"}>
+                        실시간 채팅
+                    </Text>
+                </Center>
+                <Flex
+                    flex="1"
+                    direction={"column"}
+                    overflowY={"auto"}
+                    pt={"1rem"}
+                >
+                    채팅 화면
+                </Flex>
+                <Box border={"1px"}>채팅창</Box>
+            </Flex>
         </>
-    )
+    );
 }
--- vite-project/src/pages/LiveList.tsx
+++ vite-project/src/pages/LiveList.tsx
@@ -1,9 +1,7 @@
+import MainCarouselComponent from "../components/common/MainCarouselComponent";
+
 export default function LiveList() {
-  return (
-    <div>
-      <h1>LiveList</h1>
-      <p>가장 먼저 보여지는 페이지입니다.</p>
-      
-    </div>
-  );
+    return (
+        <MainCarouselComponent />
+    )
 }
