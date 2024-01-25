c97bbfcd3a6d7e7f940d5ea696d22be4778d5652
dig04214 <dig04214@naver.com>
2024-01-25 17:53:20 +0900

    Feat: 알림 기능 추가, 네브바에 알림 기능 삽입

--- vite-project/src/components/common/notification/Nofitication.tsx
+++ vite-project/src/components/common/notification/Nofitication.tsx
@@ -11,28 +11,59 @@ import {
     Button,
     Portal,
     Box,
+    Divider,
 } from "@chakra-ui/react";
-import { useRef } from "react";
+import { BellIcon } from "@chakra-ui/icons";
+import NotificationContent from "./NotificationContent";
+
+export type NotificationContentTypes = {
+    key: string;
+    url: string;
+    msg: string;
+    time: string;
+};
 
 function Nofitication() {
+    const dummyContent: Array<NotificationContentTypes> = [
+        {
+            key: "1",
+            url: "/v1",
+            msg: "관심있는 상품의 라이브가 시작했어요!",
+            time: "01/25",
+        },
+        {
+            key: "2",
+            url: "/v1",
+            msg: "라이브가 곹 시작됩니다!",
+            time: "01/25",
+        },
+        {
+            key: "3",
+            url: "/v1",
+            msg: "라이브가 시작했어요!",
+            time: "01/25",
+        },
+    ];
     return (
-        <Popover closeOnBlur={false} placement="bottom">
+        <Popover closeOnBlur={true} placement="bottom">
             {({ isOpen, onClose }) => (
                 <>
                     <PopoverTrigger>
-                        <Button>Click to {isOpen ? "close" : "open"}</Button>
+                        <BellIcon color={"#126F54"} boxSize={6} />
                     </PopoverTrigger>
                     <Portal>
                         <PopoverContent>
-                            <PopoverHeader>This is the header</PopoverHeader>
-                            <PopoverCloseButton />
-                            <PopoverBody>
-                                <Box>
-                                    Hello. Nice to meet you! This is the body of
-                                    the popover
-                                </Box>
-                            </PopoverBody>
-                            <PopoverFooter>This is the footer</PopoverFooter>
+                            {/* <PopoverHeader>This is the header</PopoverHeader> */}
+                            {/* <PopoverCloseButton /> */}
+                            {dummyContent.map((content) => (
+                                <NotificationContent
+                                    key={content.key}
+                                    url={content.url}
+                                    msg={content.msg}
+                                    time={content.time}
+                                />
+                            ))}
+                            {/* <PopoverFooter>This is the footer</PopoverFooter> */}
                         </PopoverContent>
                     </Portal>
                 </>
