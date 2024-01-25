e426b614f919398eb34215443111450531816725
dig04214 <dig04214@naver.com>
2024-01-25 15:40:12 +0900

    Style: 판매자 라이브 페이지 컴포넌트 비율 수정

--- vite-project/src/components/broadcast/Screen.tsx
+++ vite-project/src/components/broadcast/Screen.tsx
@@ -2,14 +2,14 @@
 import { Box, Text } from "@chakra-ui/layout";
 
 function BroadcastScreen() {
-  return (
-    <Box flex="1" overflow="auto" p={6}>
-      <Text fontSize="2xl" fontWeight="bold" mb={4}>
-        방송화면
-      </Text>
-      <Box bg="gray.200" rounded="md" h="84vh" />
-    </Box>
-  );
+    return (
+        <Box w={"27%"} flex="1" overflow="auto" p={6}>
+            <Text fontSize="2xl" fontWeight="bold" mb={4}>
+                방송화면
+            </Text>
+            <Box bg="gray.200" rounded="md" h="84vh" />
+        </Box>
+    );
 }
 
 export default BroadcastScreen;
