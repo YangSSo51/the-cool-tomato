d8f2ede71f53af1ca071da69b1e40dbbdefd32d0
김현지 <hanguel97@gmail.com>
2024-01-31 10:16:55 +0900

    feat: 마이페이지 문의확인페이지

--- vite-project/src/components/mypage/seller/SellerQnaItems.tsx
+++ vite-project/src/components/mypage/seller/SellerQnaItems.tsx
@@ -77,6 +77,7 @@ function QnaItems() {
                         eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
                         minim veniam, quis nostrud exercitation ullamco laboris nisi ut
                         aliquip ex ea commodo consequat.
+                        <input type="text" />
                     </AccordionPanel>
                 </>
                 )}
