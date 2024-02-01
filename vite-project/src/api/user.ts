de8bb403c64b9b4b9cfacbabd29124877a859251
김현지 <hanguel97@gmail.com>
2024-02-01 14:04:10 +0900

    feat: 아이디찾기+비밀번호 찾기 완료

--- vite-project/src/api/user.ts
+++ vite-project/src/api/user.ts
@@ -73,4 +73,41 @@ async function registerSellerAPI(data: RegisterSeller) {
     }
 }
 
-export { loginUser, SignupUserAPI, checkIdAPI, sendEmailAPI, checkEmailAPI, registerSellerAPI };
+async function findIdAPI(data: {email: string}) {
+    console.log("아 아이디 찾을거라고~!")
+    try {
+        const response = await http.get(`${url}/find-login-id/${data.email}`)
+        const responseData = response.data;
+        if (responseData.status === 201) {
+            console.log("이메일 전송이 완료되었습니다");
+        } else if (responseData.status === 401 && responseData.divisionCode === "B005") {
+            console.log("가입된 이메일이 아닙니다 회원가입 ㄱ");
+        } else {
+            console.log("걍 문제있음")
+        }
+    } catch (error) {
+        console.error(error);
+        console.log("오류임")
+    }
+}
+
+async function findPwAPI(data: {loginId: string, email: string}) {
+    console.log("비번 찾자.. 할수잇어")
+    try {
+        const response = await http.post(`${url}/find-password`, data)
+        if (response.status === 201) {
+            console.log("비밀번호 찾기 성공");
+            return 1; // 성공을 나타내는 값 (원하는 값으로 변경 가능)
+          } else if (response.status === 400) {
+            console.log("올바른 아이디 혹은 이메일이 아님요");
+            return 0;
+          } else if (response.status === 401) {
+            console.log("아이디와 이메일의 가입정보가 일치하지 않습니다")
+          }
+        } catch (error) {
+          console.error("비밀번호 찾기 실패", error);
+          return 0;
+        }
+}
+
+export { loginUser, SignupUserAPI, checkIdAPI, sendEmailAPI, checkEmailAPI, registerSellerAPI, findIdAPI, findPwAPI };
