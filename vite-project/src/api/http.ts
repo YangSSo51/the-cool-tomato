86191ef430baf9d750504f1e56a708c6b5d344d6
dig04214 <dig04214@naver.com>
2024-02-02 10:33:13 +0900

    Refactor: 중복되는 axios 통합

--- vite-project/src/api/http.ts
+++ vite-project/src/api/http.ts
@@ -28,23 +28,13 @@ function liveAxios() {
     });
 }
 
-function productsAxios() {
+function itemAxios() {
     return axios.create({
         baseURL: "http://i10a501.p.ssafy.io:8082/v1",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
-}
-
-function ItemAxios() {
-    return axios.create({
-        baseURL: "http://3.39.6.29:8082/v1",
         headers: {
             "Content-Type": "application/json;charset=utf-8",
         },
     });
 }
 
-export { mainAxios, openViduDirectAxios, liveAxios, productsAxios, ItemAxios };
-
+export { mainAxios, openViduDirectAxios, liveAxios, productsAxios, itemAxios };
--- vite-project/src/api/itemQnA.ts
+++ vite-project/src/api/itemQnA.ts
@@ -1,6 +1,6 @@
-import { productsAxios } from "./http";
+import { itemAxios } from "./http";
 
-const http = productsAxios();
+const http = itemAxios();
 
 const url = "/product-questions";
 
@@ -11,5 +11,74 @@ async function getQnAList(params: {
 }) {
     return await http.get(url + "/list", { params });
 }
+async function sellerPutQnaAPI(params: type) {
+    try {
+        const response = await http.put(`${URL}`);
+        const responseData = response.data;
+        if (responseData.status === 200) {
+            console.log("응~잘돼");
+            return responseData;
+        } else {
+            console.log("안돼~");
+        }
+    } catch (error) {
+        console.error(error);
+        console.log("응 에러야~");
+    }
+}
 
-export { getQnAList };
+async function qnaDetailAPI(data: { productQuestionId: number }) {
+    try {
+        const response = await http.get(`${URL}/${data.productQuestionId}`);
+        const responseData = response.data;
+        if (responseData.status === 200) {
+            console.log("응~잘돼");
+            return responseData;
+        } else {
+            console.log("안돼~");
+        }
+    } catch (error) {
+        console.error(error);
+        console.log("응 에러야~");
+    }
+}
+
+async function buyerGetQnaAPI(params: type) {
+    try {
+        const response = await http.get(`${URL}/buyer/my/list`);
+        const responseData = response.data;
+        if (responseData.status === 200) {
+            console.log("응~잘돼");
+            return responseData;
+        } else {
+            console.log("안돼~");
+        }
+    } catch (error) {
+        console.error(error);
+        console.log("응 에러야~");
+    }
+}
+
+async function sellerGetQnaAPI(params: type) {
+    try {
+        const response = await http.get(`${URL}/buyer/my/list`);
+        const responseData = response.data;
+        if (responseData.status === 200) {
+            console.log("응~잘돼");
+            return responseData;
+        } else {
+            console.log("안돼~");
+        }
+    } catch (error) {
+        console.error(error);
+        console.log("응 에러야~");
+    }
+}
+
+export {
+    getQnAList,
+    sellerPutQnaAPI,
+    qnaDetailAPI,
+    buyerGetQnaAPI,
+    sellerGetQnaAPI,
+};
