33ff5907f795c60eb0c46821b8874b0651889f2e
김현종 <cowkey206@gmail.com>
2024-02-03 22:46:51 +0900

    feat: add annotation of EnableScheduling in AuthApiApplication

--- src/main/java/com/wp/AuthApiApplication.java
+++ src/main/java/com/wp/AuthApiApplication.java
@@ -2,10 +2,10 @@ package com.wp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AuthApiApplication {
 
 	public static void main(String[] args) {
