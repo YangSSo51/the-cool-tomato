08f9d47d0d7f553910680a480cae5c8dda74ed35
hyeseungS <hyeseung2000@gmail.com>
2024-02-08 12:26:20 +0900

    ✨ Feat: 채팅 금지 기능 추가

--- src/main/java/com/wp/chat/domain/chat/controller/ChatMessageController.java
+++ src/main/java/com/wp/chat/domain/chat/controller/ChatMessageController.java
@@ -1,5 +1,8 @@
 package com.wp.chat.domain.chat.controller;
 
+import com.wp.chat.domain.block.dto.response.GetBlockManageListResponse;
+import com.wp.chat.domain.block.entity.BlockManage;
+import com.wp.chat.domain.block.service.BlockManageService;
 import com.wp.chat.domain.chat.dto.request.ChatMessageRequest;
 import com.wp.chat.domain.chat.entity.ChatMessage;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +12,19 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Controller
 public class ChatMessageController {
     private final KafkaTemplate<String, ChatMessage> kafkaTemplate;
     private final NewTopic topic;
+    private final BlockManageService blockManageService;
 
     @MessageMapping("/message")
     @Transactional
     public void message(ChatMessageRequest chatMessageRequest) {
+        GetBlockManageListResponse blockManages = blockManageService.getBlockManagesBySellerId(1L);
         ChatMessage chatMessage = ChatMessage.builder().roomId(chatMessageRequest.getRoomId()).senderId(chatMessageRequest.getSenderId()).senderNickname(chatMessageRequest.getSenderNickname()).message(chatMessageRequest.getMessage()).build();
         kafkaTemplate.send(topic.name(), chatMessage);
     }
