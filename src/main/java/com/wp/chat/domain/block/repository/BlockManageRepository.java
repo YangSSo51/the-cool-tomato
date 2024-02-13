eb66c75e20d97c5425c5e46f05b99e31ef473bc5
hyeseungS <hyeseung2000@gmail.com>
2024-02-13 16:45:01 +0900

    🐛 Fix: N+1 문제 수정

--- src/main/java/com/wp/chat/domain/block/repository/BlockManageRepository.java
+++ src/main/java/com/wp/chat/domain/block/repository/BlockManageRepository.java
@@ -1,11 +1,13 @@
 package com.wp.chat.domain.block.repository;
 
 import com.wp.chat.domain.block.entity.BlockManage;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface BlockManageRepository extends JpaRepository<BlockManage, Long> {
+    @EntityGraph(attributePaths = {"blocked"})
     List<BlockManage> findAllBySellerId(Long sellerId);
     List<Long> findAllBlockedIdBySellerId(Long sellerId);
 
