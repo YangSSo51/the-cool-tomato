0ee68f00225365b5da861f44f6cdcf9d12acfb3f
= <=>
2024-02-14 02:33:31 +0900

    Fix: Bug Fix
    Fix: 주석 처리한 코드를 참조하고 있는 부분 제거
    
    Squash: 배포를 위한 커밋들 압축하기
    Fix: 주석 처리한 updateProfileThunk를 참고하고 있는 코드 주석처리
    
    Chore: http로 변경
    
    Chore: env 옵션 제거
    
    Chore: 백 통신을 https로 하도록 수정
    
    Chore: 백 주소 변경
    
    Refactor: withCredential 설정
    
    Fix: 주석 처리한 updateProfileThunk를 참고하고 있는 코드 주석처리
    
    Chore: http로 변경
    
    Chore: env 옵션 제거
    
    Chore: 백 통신을 https로 하도록 수정
    
    Refactor: withCredential 설정
    
    Refactor: axios 하나로 통합

--- vite-project/src/api/http.ts
+++ vite-project/src/api/http.ts
@@ -1,72 +1,33 @@
 import axios from "axios";
 import SockJS from "sockjs-client";
 
-const BASEURL = "211.63.208.201:8000";
-const chatWebSocketUrl = import.meta.env.PROD
-    ? "wss://" + BASEURL + "/v1/chat/ws-stomp/websocket"
-    : "ws://i10a501.p.ssafy.io:8085/v1/chat/ws-stomp/websocket";
+const BASEURL = "cool-tomato.duckdns.org";
+const credential = true;
 
-const chatbotWebSocketUrl = import.meta.env.PROD
-    ? "wss://" + BASEURL + "/stomp/chat/websocket"
-    : "ws://i10a501.p.ssafy.io:8086/stomp/chat/websocket";
+const chatWebSocketUrl = "wss://" + BASEURL + "/v1/chat/ws-stomp/websocket";
+
+const chatbotWebSocketUrl = "wss://" + BASEURL + "/stomp/chat/websocket";
 
 function mainAxios() {
     return axios.create({
-        baseURL: import.meta.env.PROD
-            ? "https://" + BASEURL + "/v1/"
-            : "http://3.39.6.29:8084/v1/",
+        baseURL: "https://" + BASEURL + "/v1",
         headers: {
             "Content-Type": "application/json;charset=utf-8",
         },
+        withCredentials: credential,
     });
 }
 
-function openViduDirectAxios() {
-    return axios.create({
-        baseURL: "https://i10a501.p.ssafy.io/openvidu/api/sessions",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: "Basic T1BFTlZJRFVBUFA6c3NhZnk",
-        },
-    });
-}
+// axios 통합
+const liveAxios = mainAxios;
+const itemAxios = mainAxios;
+const chatbotAxios = mainAxios;
+const authAxios = mainAxios;
+const chatAxios = mainAxios;
 
-function liveAxios() {
-    return axios.create({
-        baseURL: import.meta.env.PROD
-            ? "https://" + BASEURL + "/v1"
-            : "http://i10a501.p.ssafy.io:8083/v1",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
-}
-
-function itemAxios() {
-    return axios.create({
-        baseURL: import.meta.env.PROD
-            ? "https://" + BASEURL + "/v1"
-            : "http://i10a501.p.ssafy.io:8082/v1",
-        headers: {
-            "Content-Type": "application/json;charset=utf-8",
-        },
-    });
-}
-
-function chatbotAxios() {
-    return axios.create({
-        baseURL: import.meta.env.PROD
-            ? "https://" + BASEURL + "/v1"
-            : "http://i10a501.p.ssafy.io:8086/v1/",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
-}
+// SockJS
 function chatSocket() {
-    const base_url = import.meta.env.PROD
-        ? "https://" + BASEURL + "/v1"
-        : "http://i10a501.p.ssafy.io:8085/v1";
+    const base_url = "https://" + BASEURL + "/v1";
     const sock = new SockJS(`${base_url}/chat/ws-stomp`);
     sock.onclose = (ev: CloseEvent) => {
         console.log("chatting socket close");
@@ -80,9 +41,7 @@ function chatSocket() {
 }
 
 function chatbotSocket() {
-    const base_url = import.meta.env.PROD
-        ? "https://" + BASEURL
-        : "http://i10a501.p.ssafy.io:8086";
+    const base_url = "https://" + BASEURL;
     const sock = new SockJS(`${base_url}/stomp/chat`);
     sock.onclose = (ev: CloseEvent) => {
         console.log("chatting socket close");
@@ -95,33 +54,60 @@ function chatbotSocket() {
     return sock;
 }
 
-function authAxios() {
-    return axios.create({
-        baseURL: import.meta.env.PROD
-            ? "https://" + BASEURL + "/v1"
-            : "http://i10a501.p.ssafy.io:8080/v1/",
-        // 테스트용 API: 위에 서버가 작동을 안해서 테스트할 땐 아래 써주세욤
-        // baseURL: "http://211.63.208.201:8080/v1/",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
-}
+// function liveAxios() {
+//     return axios.create({
+//         baseURL: "https://" + BASEURL + "/v1",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         withCredentials: credential,
+//     });
+// }
 
-function chatAxios() {
-    return axios.create({
-        baseURL: import.meta.env.PROD
-            ? "https://" + BASEURL + "/v1"
-            : "http://i10a501.p.ssafy.io:8085/v1/",
-        headers: {
-            "Content-Type": "application/json;charset=utf-8",
-        },
-    });
-}
+// function itemAxios() {
+//     return axios.create({
+//         baseURL: "https://" + BASEURL + "/v1",
+//         headers: {
+//             "Content-Type": "application/json;charset=utf-8",
+//         },
+//         withCredentials: credential,
+//     });
+// }
+
+// function chatbotAxios() {
+//     return axios.create({
+//         baseURL: "https://" + BASEURL + "/v1",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         withCredentials: credential,
+//     });
+// }
+
+// function authAxios() {
+//     return axios.create({
+//         baseURL: "https://" + BASEURL + "/v1",
+//         // 테스트용 API: 위에 서버가 작동을 안해서 테스트할 땐 아래 써주세욤
+//         // baseURL: "http://211.63.208.201:8080/v1/",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         withCredentials: credential,
+//     });
+// }
+
+// function chatAxios() {
+//     return axios.create({
+//         baseURL: "https://" + BASEURL + "/v1",
+//         headers: {
+//             "Content-Type": "application/json;charset=utf-8",
+//         },
+//         withCredentials: credential,
+//     });
+// }
 
 export {
     mainAxios,
-    openViduDirectAxios,
     liveAxios,
     itemAxios,
     chatbotAxios,
