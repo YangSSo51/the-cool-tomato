25c956f1b46d4ae58905eff9f6ac3eff8713063b
김현종 <cowkey206@gmail.com>
2024-02-14 05:36:46 +0900

    급함

--- src/main/java/com/wp/gateway/GatewayApplication.java
+++ src/main/java/com/wp/gateway/GatewayApplication.java
@@ -2,7 +2,13 @@ package com.wp.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @EnableScheduling
