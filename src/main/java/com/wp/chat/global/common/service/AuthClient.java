56fa8602fa47c2c82f201752d4b0d20773d98097
hyeseungS <hyeseung2000@gmail.com>
2024-02-13 23:23:18 +0900

    ✨ Feat: config 설정

--- src/main/java/com/wp/chat/global/common/service/AuthClient.java
+++ src/main/java/com/wp/chat/global/common/service/AuthClient.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "auth", url = "http://i10a501.p.ssafy.io:8080/v1/auth", configuration = FeignClientConfig.class)
+@FeignClient(name = "auth", url = "http://211.63.208.201:8000//v1/auth", configuration = FeignClientConfig.class)
 public interface AuthClient {
     @PostMapping("/validationToken")
     String validateToken(@RequestBody AccessTokenRequest accessTokenRequest);
