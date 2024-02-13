fbd8caf3ff38ef8c84341929ae4abd7d6f15453b
hyeseungS <hyeseung2000@gmail.com>
2024-02-13 09:25:12 +0900

    🐛 Fix: cors에러

--- src/main/java/com/wp/chat/ChatApplication.java
+++ src/main/java/com/wp/chat/ChatApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -14,6 +17,20 @@ public class ChatApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOriginPatterns("*")
+						.allowedHeaders("*")
+						.exposedHeaders("*")
+						.allowedMethods("*")
+						.allowCredentials(true);
+			}
+		};
 	}
 
 }
