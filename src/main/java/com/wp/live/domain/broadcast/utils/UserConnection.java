76e18552996971dba696049af0b843111f4c25c0
김현종 <cowkey206@gmail.com>
2024-02-14 11:32:25 +0900

    feat: BC

--- src/main/java/com/wp/live/domain/broadcast/utils/UserConnection.java
+++ src/main/java/com/wp/live/domain/broadcast/utils/UserConnection.java
@@ -13,14 +13,14 @@ public class UserConnection {
     private WebClient webClient;
     private String url;
     public UserConnection(@Value("${user.url}")String url){
-        this.webClient = WebClient.create(url + "/v1/users/alarms");
+        this.webClient = WebClient.create(url);
         this.url = url;
     }
 
     public void registerAlarm(Long sellerId, String topicId, String nickName){
         RegisterAlarmRequestDto request = RegisterAlarmRequestDto.builder().sellerId(sellerId).alarmUrl(topicId).content(nickName + "님 라이브 방송이 시작했습니다. 지금 바로 시청해보세요!").build();
         webClient.post()
-                .uri("/validationToken")
+                .uri("/v1/users/alarms")
                 .body(Mono.just(request), RegisterAlarmRequestDto.class)
                 .retrieve()
                 .bodyToMono(ResponseDto.class)
